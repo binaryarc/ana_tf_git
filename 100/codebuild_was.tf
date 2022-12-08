9c8f162d1ef5f975e4294705d1b7e6193ee958da
jin4363 <zxcwlsgh12@naver.com>
2022-12-09 01:17:01 +0900

    Update codebuild_was.tf

--- 100/codebuild_was.tf
+++ 100/codebuild_was.tf
@@ -44,17 +44,12 @@ resource "aws_iam_role_policy" "codebuild_role_policy" {
 POLICY
 }
 
-#no need, setting github access token
-# resource "aws_codebuild_source_credential" "ana_codebuild_cred" {
-#   auth_type   = "PERSONAL_ACCESS_TOKEN"
-#   server_type = "GITHUB"
-#   token       = "gitaccesstoken"
-# }
+
 
 resource "aws_codebuild_project" "ana_cb_proj" {
   #Required
   #build project name, to be used role
-  name          = "ana_test_codebuild_project"
+  name          = "ana_test_codebuild_project_was"
   description   = "test_codebuild_project"
   #Required
   service_role  = aws_iam_role.codebuild_role_was.arn
